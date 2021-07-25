f280f08190e5f9d535823976a5c8fbba071616ee
Mark Misra <markmisra@MacBook-Air-Mark.local>
2021-07-25 16:36:32 +0300

    Add initial design

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div>
+      <div class="light">
+       </div>
+    </div>
+    <div class="clock">
+     <div class="hour">
+      <div class="hr" id="hr"></div>
+    </div>
+    <div class="min">
+      <div class="mn" id="mn">
+      </div>
+    </div>
+    <div class="sec">
+      <div class="sc" id="sc">
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
